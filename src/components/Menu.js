4f8b83b1a9daf3c65c0596ae7072a4e33ad4a7c0
Thibault Capelli <capellithibault@gmail.com>
2020-02-22 18:28:55 +0100

    fix menu bug

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,13 +10,6 @@ const MenuLink = styled(Link)`
         to === useLocation().pathname ? 'underline' : 'none'};
 `
 
-const Wrapper = styled.div`
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-`
-
 const Nav = styled.nav`
     width: 100%;
     height: 100%;
@@ -31,43 +24,41 @@ const Nav = styled.nav`
     z-index: -1;
 `
 
-const Burger = styled(HamburgerElastic)``
+const Burger = styled(HamburgerElastic)`
+    display: flex;
+    background-color: purple;
+    height: 40px;
+    border-radius: 50px;
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+`
 
 const Menu = () => {
     const [isActive, setIsActive] = useState(false)
 
     return (
-        <Wrapper>
-            <div
-                style={{
-                    backgroundColor: 'purple',
-                    borderRadius: 50,
-                    position: 'absolute',
-                    right: 10,
-                    bottom: 10,
-                }}
-            >
-                <Burger
-                    isActive={isActive}
-                    toggleButton={() => setIsActive(!isActive)}
-                    buttonWidth={22}
-                    barColor="white"
-                    buttonStyle={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        height: 40,
-                        width: 40,
-                        padding: 0,
-                    }}
-                />
-            </div>
+        <div
+            style={{
+                height: '100vh',
+                width: '100%',
+                position: 'absolute',
+                zIndex: 1,
+            }}
+        >
+            <Burger
+                isActive={isActive}
+                toggleButton={() => setIsActive(!isActive)}
+                buttonWidth={22}
+                barColor="white"
+            />
             {isActive && (
                 <Nav>
                     <MenuLink to="/">Home</MenuLink>
                     <MenuLink to="/map">Map</MenuLink>
                 </Nav>
             )}
-        </Wrapper>
+        </div>
     )
 }
 
