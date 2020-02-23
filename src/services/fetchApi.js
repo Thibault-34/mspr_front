7eab75524ef47161fe92b75809194452c55338a9
Thibault Capelli <capellithibault@gmail.com>
2020-02-23 21:32:20 +0100

    compose all requests from graphql

--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -7,7 +7,78 @@ const fetchData = () => {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                query: 'query{ pushes { id content url } }',
+                query: `query{ 
+                    events {
+                        id
+                        date
+                        artist {
+                        id
+                        name
+                        }
+                        place {
+                        id
+                        }
+                        type
+                    }
+                    artists {
+                        id
+                        name
+                        style
+                        description
+                        rsLink1
+                        rsLink2
+                        rsLink3
+                        image {
+                        fileName
+                        }
+                    }
+                    places {
+                        id
+                        name
+                        description
+                        localization {
+                        latitude
+                        longitude
+                        }
+                        category {
+                        id
+                        name
+                        }
+                    }
+                    categories {
+                        id
+                        name
+                    }
+                    articles {
+                        id
+                        title
+                        description
+                        url
+                        image {
+                        fileName
+                        }
+                    }
+                    faqs {
+                        id
+                        categoryFaq
+                        question
+                        answer
+                    }
+                    pushes {
+                        id
+                        content
+                        url
+                    }
+                    sponsors {
+                        id
+                        name
+                        description
+                        url
+                        image {
+                        fileName
+                        }
+                    }                    
+                 }`,
             }),
             mode: 'cors',
         }
